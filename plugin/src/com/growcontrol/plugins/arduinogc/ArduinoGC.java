73b72a0c010499b4d01ac10a684b38b13c2ce4df
lorenzop <lorenzo@poixson.com>
2015-08-27 22:33:47 -0400

    added lines to ArduinoGC->onEnable() to load ArduinoGC.yml config added CONFIG_PATH static variable to PluginDefines class

--- plugin/src/com/growcontrol/plugins/arduinogc/ArduinoGC.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/ArduinoGC.java
@@ -1,15 +1,36 @@
 package com.growcontrol.plugins.arduinogc;
 
 import com.growcontrol.plugins.arduinogc.commands.Commands;
+import com.growcontrol.plugins.arduinogc.configs.PluginConfig;
+import com.growcontrol.server.gcServerDefines;
 import com.growcontrol.server.plugins.gcServerPlugin;
+import com.poixson.commonapp.config.xConfigLoader;
+import com.poixson.commonjava.xLogger.xLog;
 
 
 public class ArduinoGC extends gcServerPlugin {
+	public static final String LOG_NAME = "ArduinoGC";
+
+	private volatile PluginConfig config = null;
 
 
 
 	@Override
 	protected void onEnable() {
+		// load config
+		this.config = (PluginConfig) xConfigLoader.Load(
+				PluginDefines.CONFIG_PATH,
+				PluginDefines.CONFIG_FILE,
+				PluginConfig.class,
+				true
+		);
+		if(this.config == null) {
+			this.fail("Failed to load "+PluginDefines.CONFIG_FILE);
+			return;
+		}
+		if(this.config.isFromResource())
+			xLog.getRoot(LOG_NAME).warning("Created default "+gcServerDefines.CONFIG_FILE);
+		// register listeners
 		this.register(new Commands());
 	}
 
--- plugin/src/com/growcontrol/plugins/arduinogc/PluginDefines.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/PluginDefines.java
@@ -1,11 +1,14 @@
 package com.growcontrol.plugins.arduinogc;
 
+import com.poixson.commonjava.Utils.utilsDirFile;
+
 
 public final class PluginDefines {
 	private PluginDefines() {}
 
 
 	// plugin config
+	public static final String CONFIG_PATH = utilsDirFile.mergePaths("plugins", "ArduinoGC");
 	public static final String CONFIG_FILE = "ArduinoGC.yml";
 	// config keys
 	public static final String CONFIG_VERSION = "Version";
