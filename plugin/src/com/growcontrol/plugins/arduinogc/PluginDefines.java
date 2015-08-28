72b385e2cd3b97abf0062e844904548b31ddc72d
lorenzop <lorenzo@poixson.com>
2015-08-27 21:06:48 -0400

    added PluginDefines class

---
+++ plugin/src/com/growcontrol/plugins/arduinogc/PluginDefines.java
@@ -0,0 +1,18 @@
+package com.growcontrol.plugins.arduinogc;
+
+
+public final class PluginDefines {
+	private PluginDefines() {}
+
+
+	// plugin config
+	public static final String CONFIG_FILE = "ArduinoGC.yml";
+	// config keys
+	public static final String CONFIG_VERSION = "Version";
+	// arduino hardware
+	public static final String CONFIG_HARDWARE = "Hardware";
+	public static final String CONFIG_HARDWARE_ENABLED = "Enabled";
+	public static final String CONFIG_HARDWARE_TYPE     = "Type";
+
+
+}
