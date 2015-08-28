ba30801b60bb19f3cead67abf72fa8ab716150f0
lorenzop <lorenzo@poixson.com>
2015-08-27 21:07:39 -0400

    added empty onEnable() and onDisable() functions to gui/ArduinoGC class

--- plugin/src/com/growcontrol/plugins/arduinogc/gui/ArduinoGC.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/gui/ArduinoGC.java
@@ -1,7 +1,21 @@
 package com.growcontrol.plugins.arduinogc.gui;
 
+import com.growcontrol.client.plugins.gcClientPlugin;
 
-public class ArduinoGC {
+
+public class ArduinoGC extends gcClientPlugin {
+
+
+
+	@Override
+	protected void onEnable() {
+	}
+
+
+
+	@Override
+	protected void onDisable() {
+	}
 
 
 
