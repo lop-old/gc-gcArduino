13cdebb93ba49f35343531f3fab0944984315e47
lorenzop <lorenzo@poixson.com>
2015-09-02 14:26:09 -0400

    merged FindCommsWatcher class into FindComms class

--- plugin/src/com/growcontrol/plugins/arduinogc/server/connections/usb/FindComms.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/server/connections/usb/FindComms.java
@@ -1,4 +1,4 @@
-package com.growcontrol.plugins.arduinogc.server.connections;
+package com.growcontrol.plugins.arduinogc.server.connections.usb;
 
 import gnu.io.CommPortIdentifier;
 
@@ -6,33 +6,138 @@ import java.util.Collections;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import com.poixson.commonjava.Utils.CoolDown;
+import com.poixson.commonjava.Utils.utils;
+import com.poixson.commonjava.xLogger.xLog;
 
 
 public final class FindComms {
-	private FindComms() {}
+
+	private static volatile FindComms instance = null;
+	private static final Object instanceLock = new Object();
+
+	private final Map<String, CommPortIdentifier> cached =
+			new HashMap<String, CommPortIdentifier>();
+	private final Set<EventListenerUSB> listeners =
+			new CopyOnWriteArraySet<EventListenerUSB>();
+
+	private final Object updateLock = new Object();
+	private final CoolDown cool = CoolDown.get("2s");
 
 
 
-	public static Map<String, CommPortIdentifier> search() {
-		final Map<String, CommPortIdentifier> map =
-				new HashMap<String, CommPortIdentifier>();
-		final Enumeration<?> ports = CommPortIdentifier.getPortIdentifiers();
-		while(ports.hasMoreElements()) {
-			final CommPortIdentifier port = (CommPortIdentifier) ports.nextElement();
-			switch (port.getPortType()) {
-			case CommPortIdentifier.PORT_SERIAL:
-				map.put(port.getName(), port);
-				break;
-			case CommPortIdentifier.PORT_RS485:
-			case CommPortIdentifier.PORT_PARALLEL:
-			case CommPortIdentifier.PORT_I2C:
-			case CommPortIdentifier.PORT_RAW:
-				break;
-			default:
-				throw new RuntimeException("Unknown port type: "+Integer.toString(port.getPortType()));
+	// get instance
+	public static FindComms get() {
+		if(instance == null) {
+			synchronized(instanceLock) {
+				if(instance == null)
+					instance = new FindComms();
 			}
 		}
-		return Collections.unmodifiableMap(map);
+		return instance;
+	}
+
+
+
+	// get by port name
+	public static CommPortIdentifier get(final String portName) {
+		return get()
+			.getByName(portName);
+	}
+	public CommPortIdentifier getByName(final String portName) {
+		if(utils.isEmpty(portName)) throw new NullPointerException("portName argument is required!");
+		// get available comm ports
+		final Map<String, CommPortIdentifier> map = this.getAllPorts();
+		if(utils.isEmpty(map))
+			return null;
+		return map.get(portName.toLowerCase());
+	}
+
+
+
+	// get all ports
+	public static Map<String, CommPortIdentifier> getAll() {
+		return get()
+			.getAllPorts();
+	}
+	public Map<String, CommPortIdentifier> getAllPorts() {
+		synchronized(this.updateLock) {
+			if(!this.cool.runAgain())
+				return Collections.unmodifiableMap(this.cached);
+			final Set<String> lastSet = this.cached.keySet();
+			this.cached.clear();
+			final EventListenerUSB[] listeners = this.listeners.toArray(new EventListenerUSB[0]);
+			final Enumeration<?> idents = CommPortIdentifier.getPortIdentifiers();
+			while(idents.hasMoreElements()) {
+				final CommPortIdentifier port = (CommPortIdentifier) idents.nextElement();
+				final String portName = port.getName();
+				final int    portType = port.getPortType();
+				switch(portType) {
+				case CommPortIdentifier.PORT_SERIAL:
+					this.cached.put(portName, port);
+					break;
+				case CommPortIdentifier.PORT_RS485:
+				case CommPortIdentifier.PORT_PARALLEL:
+				case CommPortIdentifier.PORT_I2C:
+				case CommPortIdentifier.PORT_RAW:
+					break;
+				default:
+					throw new RuntimeException("Unknown port type: "+
+							Integer.toString(portType));
+				}
+				// trigger added
+				if(!this.cached.containsKey(portName)) {
+					this.log().info("New comm port detected: "+portName);
+					for(final EventListenerUSB listener : listeners) {
+						listener.added(portName, port);
+					}
+				}
+			} // end while
+			// trigger removed
+			if(lastSet.isEmpty()) {
+				for(final String portName : lastSet) {
+					this.log().warning("Comm port removed: "+portName);
+					for(final EventListenerUSB listener : listeners) {
+						listener.removed(portName);
+					}
+				}
+			}
+		} // end synchronized
+		return Collections.unmodifiableMap(this.cached);
+	}
+
+
+
+	public FindComms() {
+	}
+
+
+
+	public void addListener(final EventListenerUSB listener) {
+		final CommPortIdentifier[] current;
+		synchronized(this.updateLock) {
+			current = (CommPortIdentifier[]) this.listeners.toArray(new EventListenerUSB[0]);
+			this.listeners.add(listener);
+		}
+		for(final CommPortIdentifier ident : current) {
+			listener.added(ident.getName(), ident);
+		}
+	}
+	public void removeListener(final EventListenerUSB listener) {
+		this.listeners.remove(listener);
+	}
+	public void clearListener() {
+		this.listeners.clear();
+	}
+
+
+
+	// logger
+	public xLog log() {
+		return xLog.getRoot();
 	}
 
 
