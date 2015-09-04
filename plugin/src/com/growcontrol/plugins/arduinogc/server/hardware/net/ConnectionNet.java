366624e919c3692b9f060382ffd628f702653e19
lorenzop <lorenzo@poixson.com>
2015-09-04 16:32:27 -0400

    updated ConnectionNet class (unfinished)

--- plugin/src/com/growcontrol/plugins/arduinogc/server/hardware/net/ConnectionNet.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/server/hardware/net/ConnectionNet.java
@@ -1,17 +1,69 @@
 package com.growcontrol.plugins.arduinogc.server.hardware.net;
 
 import com.growcontrol.plugins.arduinogc.server.hardware.ArduinoConnection;
+import com.growcontrol.plugins.arduinogc.server.hardware.serial.ConnectionSerial;
+import com.poixson.commonjava.Utils.utils;
+import com.poixson.commonjava.Utils.xHashable;
 
 
 public class ConnectionNet extends ArduinoConnection {
 
+	// socket info
+	protected final String host;
+	protected final int    port;
+	public final String key;
+
+	// netty socket
+//TODO:
+
+	private volatile boolean stopping = false;
+
+
+
+	public ConnectionNet(final String host, final int port) {
+		if(utils.isEmpty(host)) throw new NullPointerException("host argument is required!");
+		this.host = host;
+		this.port = port;
+		this.key = this.genKey();
+
+//TODO:
+
+		log().info("Connecting to.. "+this.key);
+	}
+
 
 
 	@Override
-	public void Process() {
+	public void close() {
+		this.stopping = true;
 	}
 	@Override
-	public void Send() {
+	public boolean isClosed() {
+		return this.stopping;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return this.key;
+	}
+	@Override
+	public String getKey() {
+		return this.key;
+	}
+	private String genKey() {
+		return (new StringBuilder())
+				.append(this.host)
+				.append(':')
+				.append(this.port)
+				.toString();
+	}
+	@Override
+	public boolean matches(final xHashable hashable) {
+		if(hashable == null || !(hashable instanceof ConnectionSerial))
+			return false;
+		return this.getKey().equalsIgnoreCase(hashable.getKey());
 	}
 
 
