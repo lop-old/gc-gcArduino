cab2870c9344bb37c63733bf3053338ac3b0a39f
lorenzop <lorenzo@poixson.com>
2015-09-04 16:32:01 -0400

    rewrote ConnectionSerial class to use java-simple-serial-connector library finished ArduinoConnection class

--- plugin/src/com/growcontrol/plugins/arduinogc/server/hardware/ArduinoConnection.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/server/hardware/ArduinoConnection.java
@@ -1,12 +1,168 @@
 package com.growcontrol.plugins.arduinogc.server.hardware;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
-//TODO: should this be an interface?
-public abstract class ArduinoConnection {
+import com.growcontrol.plugins.arduinogc.PluginDefines;
+import com.growcontrol.plugins.arduinogc.server.ArduinoGC;
+import com.growcontrol.plugins.arduinogc.server.configs.HardwareConfig;
+import com.growcontrol.plugins.arduinogc.server.configs.HardwareConfigNet;
+import com.growcontrol.plugins.arduinogc.server.configs.HardwareConfigSerial;
+import com.growcontrol.plugins.arduinogc.server.hardware.net.ConnectionNet;
+import com.growcontrol.plugins.arduinogc.server.hardware.serial.ConnectionSerial;
+import com.poixson.commonjava.Utils.utils;
+import com.poixson.commonjava.Utils.xCloseable;
+import com.poixson.commonjava.Utils.xHashable;
+import com.poixson.commonjava.xLogger.xLog;
 
 
-	public abstract void Process();
-	public abstract void Send();
+public abstract class ArduinoConnection implements xCloseable, xHashable {
+	private static final String LOG_NAME = ArduinoGC.LOG_NAME;
+
+	private static final Map<String, ArduinoConnection> connections =
+			new ConcurrentHashMap<String, ArduinoConnection>();
+
+//TODO: not used yet
+//	protected static final xTime HEARTBEAT = xTime.get("30s");
+//	protected static final xTime THREAD_WAIT = xTime.get(100L);
+
+//TODO: not used yet
+//	protected final Map<Integer, ArduinoPin> pinsOut = new HashMap<Integer, ArduinoPin>();
+//	protected final msgQueue queue = new msgQueue();
+
+
+
+	public static ArduinoConnection get(final HardwareConfig config) {
+		final String key = config.key;
+		// existing connection
+		{
+			final ArduinoConnection connect = connections.get(key);
+			if(connect != null)
+				return connect;
+		}
+		// new connection
+		synchronized(connections) {
+			if(connections.containsKey(key))
+				return connections.get(key);
+			final ArduinoConnection connect = ArduinoConnection.load(config);
+			if(connect == null)
+				return null;
+			connections.put(key, connect);
+			return connect;
+		}
+	}
+	private static ArduinoConnection load(final HardwareConfig config) {
+		if(ArduinoGC.isStopping())
+			return null;
+		// serial config
+		if(config instanceof HardwareConfigSerial) {
+			final HardwareConfigSerial cfg = (HardwareConfigSerial) config;
+			final String portName = cfg.port;
+			final int    baud     = cfg.baud;
+			return new ConnectionSerial(
+					portName,
+					baud
+			);
+		}
+		// net config
+		if(config instanceof HardwareConfigNet) {
+			final HardwareConfigNet cfg = (HardwareConfigNet) config;
+			final String host = cfg.host;
+			final int    port = cfg.port;
+			return new ConnectionNet(
+					host,
+					port
+			);
+		}
+		// unknown config
+		throw new RuntimeException("Unknown hardware config type: "+
+				config.getClass().getName());
+	}
+	public static Set<ArduinoConnection> loadAll(
+			final Map<String, HardwareConfig> configs) {
+		if(utils.isEmpty(configs)) {
+			log().fine("No arduino devices found in "+PluginDefines.CONFIG_FILE);
+			return null;
+		}
+		// load from configs
+		log().info("Connecting to hardware..");
+		final Set<ArduinoConnection> connections = new HashSet<ArduinoConnection>();
+		int count  = 0;
+		int failed = 0;
+		for(final HardwareConfig cfg : configs.values()) {
+			ArduinoConnection connect = null;
+			try {
+				connect = ArduinoConnection.get(cfg);
+			} catch (Exception e) {
+				connect = null;
+				log().trace(e);
+			}
+			if(connect == null) {
+				failed++;
+				log().warning("Failed to open comm port: "+cfg.getKey());
+			} else {
+				count++;
+				log().info("Successfully connected to: "+cfg.getKey());
+			}
+			connections.add(connect);
+		}
+		if(count > 0)
+			log().info("Successfully connected to [ "+Integer.toString(count)+" ] devices!");
+		if(failed > 0)
+			log().warning("Failed connecting to [ "+Integer.toString(failed)+" ] devices!");
+		return connections;
+	}
+
+
+
+	public static void Close(final ConnectionSerial connect) {
+		utils.safeClose(connect);
+		synchronized(connections) {
+			connections.remove(connect.getKey());
+		}
+	}
+	public static void Close(final String portName) {
+		if(!connections.containsKey(portName))
+			return;
+		synchronized(connections) {
+			final ArduinoConnection connect = connections.get(portName);
+			if(connect == null)
+				return;
+			utils.safeClose(connect);
+			connections.remove(portName);
+		}
+	}
+	public static void CloseAll() {
+		if(connections.isEmpty())
+			return;
+		synchronized(connections) {
+			final Iterator<ArduinoConnection> it = connections.values().iterator();
+			while(it.hasNext()) {
+				utils.safeClose(it.next());
+				it.remove();
+			}
+			connections.clear();
+		}
+	}
+
+
+
+//	public abstract void Process();
+//	public abstract void Send();
+
+
+
+	// logger
+	private static volatile xLog _log = null;
+	public static xLog log() {
+		if(_log == null)
+			_log = xLog.getRoot(LOG_NAME);
+		return _log;
+	}
+
 
 
 }
--- plugin/src/com/growcontrol/plugins/arduinogc/server/hardware/serial/ConnectionSerial.java
+++ plugin/src/com/growcontrol/plugins/arduinogc/server/hardware/serial/ConnectionSerial.java
@@ -1,17 +1,19 @@
 package com.growcontrol.plugins.arduinogc.server.hardware.serial;
 
-import gnu.io.CommPortIdentifier;
-import gnu.io.SerialPortEvent;
-import gnu.io.SerialPortEventListener;
+import java.nio.charset.Charset;
 
+import jssc.SerialPort;
+import jssc.SerialPortEvent;
+import jssc.SerialPortEventListener;
+import jssc.SerialPortException;
+
+import com.growcontrol.plugins.arduinogc.PluginDefines;
 import com.growcontrol.plugins.arduinogc.server.hardware.ArduinoConnection;
 import com.poixson.commonjava.Utils.utils;
+import com.poixson.commonjava.Utils.xHashable;
 
 
 //	private static final int TIMEOUT = 2000;
-//	private static final int ASCII_SPACE   = ' ';
-//	private static final int ASCII_DASH    = '-';
-//	private static final int ASCII_NEWLINE = '\n';
 //
 //	private Enumeration ports = null;
 //
@@ -25,43 +27,182 @@ import com.poixson.commonjava.Utils.utils;
 //	private OutputStream out = null;
 //
 //	private boolean connected = false;
+//TODO: will need to expect exceptions at every call, in case a port is disconnected
 public class ConnectionSerial extends ArduinoConnection implements SerialPortEventListener {
 
-	protected volatile String             portName  = null;
-	protected volatile CommPortIdentifier portIdent = null;
-	protected final int baud;
+	// port info
+	private final String portName;
+	private final int    baud;
+
+	// comm port
+	private final SerialPort serial;
+
+	private volatile String buffer = "";
+	private volatile boolean stopping = false;
 
 
 
 	public ConnectionSerial(final String portName, final int baud) {
-		if(utils.isEmpty(portName)) throw new NullPointerException("port argument is required!");
-		this.portIdent = FindComms.get(portName);
-		if(this.portIdent == null)
-			this.portName = portName;
-		else
-			this.portName = this.portIdent.getName();
-		this.baud = baud;
+		if(utils.isEmpty(portName)) throw new NullPointerException("portName argument is required!");
+		if(!isValidBaud(baud))      throw new IllegalArgumentException("Invalid baud rate: "+Integer.toString(baud));
+		this.portName = portName;
+		this.baud     = baud;
+		// open comm port
+		this.serial = new SerialPort(this.portName);
+		try {
+			this.serial.openPort();
+			this.serial.setParams(
+					this.baud,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE
+			);
+			this.serial.addEventListener(this);
+			this.serial.setEventsMask(SerialPort.MASK_RXCHAR);
+			this.send("scan");
+		} catch (SerialPortException e) {
+			log().severe("Failed to open comm port: "+this.portName);
+			log().trace(e);
+			throw new RuntimeException("Failed to find comm port: "+this.portName);
+		}
 	}
-	public ConnectionSerial(final CommPortIdentifier portIdent, final int baud) {
-		if(portIdent == null) throw new NullPointerException("portIdent argument is required!");
-		this.portIdent = portIdent;
-		this.portName  = portIdent.getName();
-		this.baud = baud;
+
+
+
+	public void send(final String msg) throws SerialPortException {
+		final byte[] bytes = (new StringBuilder())
+				.append(msg)
+				.append(PluginDefines.SERIAL_LINE_ENDING)
+				.toString().getBytes(Charset.forName("US-ASCII"));
+		this.serial.writeBytes(bytes);
 	}
 
 
 
 	@Override
-	public void Process() {
+	public void close() {
+		this.stopping = true;
+		try {
+			this.serial.closePort();
+		} catch (SerialPortException e) {
+			log().trace(e);
+		}
 	}
 	@Override
-	public void Send() {
+	public boolean isClosed() {
+		return this.stopping;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return this.portName;
+	}
+	@Override
+	public String getKey() {
+		return this.portName;
+	}
+	@Override
+	public boolean matches(final xHashable hashable) {
+		if(hashable == null || !(hashable instanceof ConnectionSerial))
+			return false;
+		return this.getKey().equalsIgnoreCase(hashable.getKey());
+	}
+
+
+
+	public void Process(final String line) {
+System.out.println("LINE: "+line);
+	}
+
+
+
+	private void bufferAppend(final byte[] bytes) {
+		synchronized(this.buffer) {
+			this.buffer += (new String(bytes));
+			final String line = this.bufferGetLine();
+			if(line != null) {
+				this.Process(line);
+			}
+		}
+	}
+	private String bufferGetLine() {
+		if(this.buffer.isEmpty())
+			return null;
+		final String line;
+		synchronized(this.buffer) {
+			int pos = Integer.MAX_VALUE;
+			for(final char c : PluginDefines.SERIAL_LINE_ENDINGS) {
+				final int p = this.buffer.indexOf(c);
+				if(p == -1)
+					continue;
+				if(pos > p) pos = p;
+				if(pos == 0)
+					break;
+			}
+			if(pos == 0) {
+				this.buffer = this.buffer.substring(1);
+				return null;
+			}
+			if(pos == Integer.MAX_VALUE)
+				return null;
+			line        = this.buffer.substring(0, pos);
+			this.buffer = this.buffer.substring(pos + 1);
+		}
+		return line;
 	}
 
 
 
 	@Override
 	public void serialEvent(final SerialPortEvent event) {
+		// data is available
+		if(event.isRXCHAR()) {
+			final byte[] buf;
+			try {
+				buf = this.serial.readBytes(event.getEventValue());
+				if(utils.isEmpty(buf))
+					return;
+				this.bufferAppend(buf);
+			} catch (SerialPortException e) {
+				log().trace(e);
+			}
+//		} else
+//		// CTS line changed state
+//		if(event.isCTS()) {
+//			log().info("CTS: "+(event.getEventValue() == 1 ? "ON" : "off"));
+//		} else
+//		// DSR line changed state
+//		if(event.isDSR()) {
+//			log().info("DSR: "+(event.getEventValue() == 1 ? "ON" : "off"));
+		} else {
+			log().warning("Unknown serial event type! "+event.getEventType());
+		}
+	}
+
+
+
+	public static boolean isValidBaud(final int baud) {
+		switch(baud) {
+		case SerialPort.BAUDRATE_110:
+		case SerialPort.BAUDRATE_300:
+		case SerialPort.BAUDRATE_600:
+		case SerialPort.BAUDRATE_1200:
+		case SerialPort.BAUDRATE_4800:
+		case SerialPort.BAUDRATE_9600:
+		case SerialPort.BAUDRATE_14400:
+		case SerialPort.BAUDRATE_19200:
+		case SerialPort.BAUDRATE_38400:
+		case SerialPort.BAUDRATE_57600:
+		case SerialPort.BAUDRATE_115200:
+		case SerialPort.BAUDRATE_128000:
+		case SerialPort.BAUDRATE_256000:
+			return true;
+		default:
+			break;
+		}
+		return false;
 	}
 
 
